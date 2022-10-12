cfb7d84f36d2c64b3da0eb9b0c6fc0befabe1973
jinx71 <karim.sazed@gmail.com>
2022-10-12 09:32:04 +0000

    feat: add ProtectedRoute component

---
+++ coinscope/client/src/components/ProtectedRoute.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+import Spinner from './Spinner';
+
+const ProtectedRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="min-h-[40vh] flex items-center justify-center">
+        <Spinner label="Checking your session…" />
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
